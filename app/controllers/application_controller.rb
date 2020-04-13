07d13d9ad769030798aff79029f2db9f0b9946e5
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-04-13 22:41:23 +0700

    use shard1 for request with region vietnam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  around_action :switch_shard
+
+  def switch_shard
+    if params[:region] == "vietnam"
+      ActiveRecord::Base.connected_to(
+          database: { writing: :shard1, reading: :shard1_replica }
+      ) do
+        yield
+      end
+    else
+      #default is primary and primary replica
+      yield
+    end
+  end
 end
