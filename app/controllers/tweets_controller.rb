24a5fba7616074d212fc23453f3696e6dd83c441
Tien Nguyen <tien.nguyen@reddoorz.com>
2020-04-13 18:22:45 +0700

    create action retweet and destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,31 @@
 class TweetsController < ApplicationController
+  before_action :find_tweet,  only: [:destroy, :retweet]
   def index
     return render :json => Tweet.all.order(:retweets).limit(10) if params[:format] == 'json'
+  end
+
+  def destroy
+    if @tweet
+      @tweet.delete
+      render json: {success: true}
+    else
+      render json: {success: false}
+    end
+  end
+
+  def retweet
+    if @tweet
+      @tweet.do_retweet
+      render json: {success: true, tweet: @tweet}
+    else
+      render json: {success: false}
+    end
+  end
+
+  private
+
+  def find_tweet
+    tweet_id = params[:id] || params[:tweet_id]
+    @tweet = Tweet.find(tweet_id)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,11 @@
 class Tweet < ApplicationRecord
+  validates_length_of :content, maximum: 140
+
+  def do_retweet
+    transaction do
+      Tweet.create(content: content)
+      update!(retweets: retweets + 1)
+    end
+
+  end
 end
